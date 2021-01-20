111d1e373fe27e6361964549355f09771e396f47
Bruno Fernando Feitoza <bfeitoza634cyt@gmail.com>
2021-01-19 21:55:13 -0300

    feat: add first section

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,5 @@
 const Layout: React.FC = ({ children }) => {
-  return (
-    <main className="max-w-6xl mx-auto">{children}</main>
-  )
-}
+  return <main className="max-w-6xl mx-auto">{children}</main>;
+};
 
-export default Layout
+export default Layout;
