60bc392c4e713c04fda6da573a59989ba71e2212
Bruno Fernando Feitoza <bfeitoza634cyt@gmail.com>
2021-05-31 07:45:28 -0300

    chore: setup animations on tailwind.config.js

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # UI Clone - Discord Web
 
-## ⁉ Sobre
+## Sobre
 
 O objetivo deste projeto é aprender e fixar os conceitos das tecnologias **Next.js**, **Tailwind CSS** e um pouco do **Framer Motion**.
 
 ![Demo](./.github/images/demo.gif)
 
-## 🛠 Tecnologias
+## Tecnologias
 
 Tecnologias utilizadas no desenvolvimento deste projeto.
 
@@ -15,7 +15,8 @@ Tecnologias utilizadas no desenvolvimento deste projeto.
 - [TypeScript](https://www.typescriptlang.org/)
 - [Tailwind CSS](https://tailwindcss.com/docs/font-weight)
 - [Framer Motion](https://www.framer.com/motion/)
-## 💻 Como rodar
+
+## Como rodar
 
 **Faça um clone deste repositório e acesse a pasta do projeto**
 
@@ -35,7 +36,7 @@ $ yarn dev
 # Pronto! O projeto está rodando no endereço: http://localhost:3000.
 ```
 
-## 📝 Licença
+## Licença
 
 Este projeto está licenciado sob a Licença **MIT** - consulte o arquivo [LICENSE](LICENSE) para obter detalhes.
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,39 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'shift-right-3LU1WH-685s': 'shift-right-3LU1WH 685s linear forwards',
+        'shift-right-3LU1WH-960s': 'shift-right-3LU1WH 960s linear forwards',
+        'shift-right-3LU1WH-1200s': 'shift-right-3LU1WH 1200s linear forwards',
+        'shift-right-3LU1WH-1600s': 'shift-right-3LU1WH 1600s linear forwards',
+        'shift-right-3LU1WH-2400s': 'shift-right-3LU1WH 2400s linear forwards',
+        'shift-right-3LU1WH-4800s': 'shift-right-3LU1WH 4800s linear forwards',
+        'hello-2FR9NG': 'hello-2FR9NG 100s linear 180s forwards',
+      },
+      keyframes: {
+        'shift-right-3LU1WH': {
+          '0%': {
+            '-webkit-transform': 'translateX(0)',
+            transform: 'translateX(0)'
+          },
+          'to': {
+            '-webkit-transform': 'translateX(8000px)',
+            transform: 'translateX(8000px)'
+          }
+        },
+        'hello-2FR9NG': {
+          '0%': {
+            '-webkit-transform': 'translateY(0)',
+            transform: 'translateY(0)'
+          },
+          'to': {
+            '-webkit-transform': 'translateY(-2000px)',
+            transform: 'translateY(-2000px)'
+          }
+        }
+      }
+    },
   },
   variants: {
     extend: {},
